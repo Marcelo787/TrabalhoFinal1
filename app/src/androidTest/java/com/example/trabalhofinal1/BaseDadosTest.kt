ea186afef2f358145c788e62f78aed478917ea58
Marcelo Coutinho <marcelocoutinho777@hotmail.com>
2022-06-16 21:31:26 +0100

    Criado teste para verificar se é possivel alterar Clientes, Mercadorias, Motoristas e Viagens.

--- app/src/androidTest/java/com/example/trabalhofinal1/BaseDadosTest.kt
+++ app/src/androidTest/java/com/example/trabalhofinal1/BaseDadosTest.kt
@@ -1,8 +1,10 @@
 package com.example.trabalhofinal1
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.Assert.assertEquals
 import junit.framework.Assert.assertTrue
 
 import org.junit.Assert.assertNotEquals
@@ -17,7 +19,7 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class BaseDadosTest {
-    fun appContext() =
+    private fun appContext() =
         InstrumentationRegistry.getInstrumentation().targetContext
 
     private fun getWritableDatabase(): SQLiteDatabase {
@@ -108,6 +110,110 @@ class BaseDadosTest {
         viagem.id = TabelaBDViagem(db).insert(viagem.toContentValues())
 
         assertNotEquals(-1,viagem.id)
+        db.close()
+    }
+
+    @Test
+    fun consegueAlterarCliente() {
+        val db = getWritableDatabase()
+
+        val cliente = Cliente("Teste","teste","teste")
+        insereCliente(db, cliente)
+
+        cliente.nome = "Transjanardo"
+        cliente.cc = "23564897"
+        cliente.telemovel = "912546897"
+
+        val registosAlterados = TabelaBDCliente(db).update(
+            cliente.toContentValues(),
+            "${BaseColumns._ID}=?",
+            arrayOf("${cliente.id}"))
+
+        assertEquals(1,registosAlterados)
+
+        db.close()
+    }
+
+    @Test
+    fun consegueAlterarMotorista() {
+        val db = getWritableDatabase()
+
+        val motorista = Motorista("Teste","teste","teste","teste","teste","teste@gmail.com")
+        insereMotorista(db, motorista)
+
+        motorista.nome = "João Silva"
+        motorista.dataNascimento = "1-janeiro-1990"
+        motorista.morada = "Aveiro"
+        motorista.cc = "23546987"
+        motorista.telemovel = "915468276"
+        motorista.email = "joaosilva@gmail.com"
+
+        val registosAlterados = TabelaBDMotorista(db).update(
+            motorista.toContentValues(),
+            "${BaseColumns._ID}=?",
+            arrayOf("${motorista.id}"))
+
+        assertEquals(1,registosAlterados)
+
+        db.close()
+    }
+
+    @Test
+    fun consegueAlterarMercadoria() {
+        val db = getWritableDatabase()
+
+        val cliente = Cliente("Teste2","23165487","915423659")
+        insereCliente(db, cliente)
+
+        val mercadoria = Mercadoria("teste","teste","teste", cliente.id)
+        insereMercadoria(db, mercadoria)
+
+        mercadoria.tipoMercadoria = "Madeira"
+        mercadoria.peso = "45000kg"
+        mercadoria.dimensoes = "190"
+        mercadoria.clienteId = cliente.id
+
+        val registosAlterados = TabelaBDMotorista(db).update(
+            mercadoria.toContentValues(),
+            "${BaseColumns._ID}=?",
+            arrayOf("$mercadoria.id}"))
+
+        assertEquals(1,registosAlterados)
+
+        db.close()
+    }
+
+    @Test
+    fun consegueAlterarViagem() {
+        val db = getWritableDatabase()
+
+        val cliente = Cliente("TranSilva","32516454","915423659")
+        insereCliente(db, cliente)
+
+        val mercadoria = Mercadoria("Peças Automóveis","20000kg","915", cliente.id)
+        insereMercadoria(db, mercadoria)
+
+        val motorista = Motorista("António Marques","22-jun-1997","Viseu","23562458","914523658","antoniomarques@gmail.com")
+        insereMotorista(db, motorista)
+
+        val viagem = Viagem("datateste","datateste2","Aveiro","Lisboa","Express", mercadoria.id, motorista.id)
+        insereViagem(db, viagem)
+
+        viagem.dataPartida = "12-jan-2022"
+        viagem.dataChegada = "12-jan-2022"
+        viagem.localPartida = "Aveiro"
+        viagem.localChegada = "Viseu"
+        viagem.tipoServico = "express"
+        viagem.mercadoriaId = mercadoria.id
+        viagem.motoristaId = motorista.id
+
+        val registosAlterados = TabelaBDViagem(db).update(
+            viagem.toContentValues(),
+            "${BaseColumns._ID}=?",
+            arrayOf("$viagem.id}"))
+
+        assertEquals(1,registosAlterados)
+
         db.close()
     }
 }
