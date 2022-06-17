0b2e85b528a8bbf233f98970ecfac7b07925228b
Marcelo Coutinho <marcelocoutinho777@hotmail.com>
2022-06-17 10:58:24 +0100

    Criada TabelaBDChegada

---
+++ app/src/main/java/com/example/trabalhofinal1/TabelaBDChegada.kt
@@ -0,0 +1,20 @@
+package com.example.trabalhofinal1
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDChegada (db: SQLiteDatabase) : TabelaBD(db, NOME){
+    override fun cria() {
+        db.execSQL(
+            "CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " $CAMPO_LOCAL_ENTREGA TEXT NOT NULL," +
+                    " $CAMPO_DATA_CHEGADA TEXT NOT NULL)"
+        )
+    }
+
+    companion object {
+        const val NOME = "chegada"
+        const val CAMPO_LOCAL_ENTREGA = "localChegada"
+        const val CAMPO_DATA_CHEGADA = "dataChegada"
+    }
+}
