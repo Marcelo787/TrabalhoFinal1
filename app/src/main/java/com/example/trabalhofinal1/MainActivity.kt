847e285f34ecacbadca0751aa9ab4029d4cb9884
Marcelo Coutinho <marcelocoutinho777@hotmail.com>
2022-07-05 23:58:45 +0100

    Ajustes

--- app/src/main/java/com/example/trabalhofinal1/MainActivity.kt
+++ app/src/main/java/com/example/trabalhofinal1/MainActivity.kt
@@ -2,10 +2,92 @@ package com.example.trabalhofinal1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.trabalhofinal1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    var idMenuAtual = R.menu.menu_main
+        get() = field
+        set(value) {
+            if (value != field) {
+                field = value
+                invalidateOptionsMenu()
+            }
+        }
+
+    var menu: Menu? = null
+
+    var fragment: Fragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(idMenuAtual, menu)
+        this.menu = menu
+        return true
+    }
+
+    /*
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        val opcaoProcessada: Boolean
+
+        if (fragment is MenuPrincipalFragment) {
+            opcaoProcessada = (fragment as MenuPrincipalFragment).processaOpcaoMenu(item)
+        } else if (fragment is ListaViagemFragment) {
+            opcaoProcessada = (fragment as ListaLivrosFragment).processaOpcaoMenu(item)
+        } else if (fragment is EditarLivroFragment) {
+            opcaoProcessada = (fragment as EditarLivroFragment).processaOpcaoMenu(item)
+        } else if (fragment is EliminarLivroFragment) {
+            opcaoProcessada = (fragment as EliminarLivroFragment).processaOpcaoMenu(item)
+        } else {
+            opcaoProcessada = false
+        }
+
+        if (opcaoProcessada) return true
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    fun mostraOpcoesAlterarEliminar(mostra: Boolean) {
+        menu!!.findItem(R.id.action_alterar).setVisible(mostra)
+        menu!!.findItem(R.id.action_eliminar).setVisible(mostra)
+    }
+
+    fun atualizaTitulo(id_string_titulo: Int) {
+        binding.toolbar.setTitle(id_string_titulo)
     }
 }
